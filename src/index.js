60003f673ea1b605118d557dd428b30cacf17d9b
Marina8520 <4079722@mail.ru>
2022-02-06 16:00:09 +0300

    feat: add commint human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function toReadable (number) {
-  
+    let units = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifthteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+      ];
+      let dozens = [
+        "twenty",
+        "thirty",
+        "fourty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+      ];
+      
+      let numberSting, numberDozens, numberUnits, numberHundreds, numberThuosands;
+      
+      if (number < 20) {
+        numberSting = units[number - 1];
+      } else if (number < 100) {
+        numberDozens = Math.floor(number / 10);
+        numberUnits = number % 10;
+        if (numberUnits !== 0) {
+          numberSting = dozens[numberDozens - 2] + " " + units[numberUnits - 1];
+        } else {
+          numberSting = dozens[numberDozens - 2];
+        }
+      } else if (number < 1000) {
+        numberHundreds = Math.floor(number / 100);
+        numberDozens = Math.floor(number / 10) % 10;
+        numberUnits = number % 10;
+        if (numberUnits !== 0 && numberDozens !== 0) {
+          numberSting = units[numberHundreds - 1] + " hundred " + dozens[numberDozens - 2] + " " + units[numberUnits - 1];
+        } else if (numberUnits == 0 && numberDozens !== 0) {
+          numberSting = units[numberHundreds - 1] + " hundred " + dozens[numberDozens - 2];
+        } else if (numberUnits !== 0 && numberDozens == 0) {
+          numberSting = units[numberHundreds - 1] + " hundred " + " " + units[numberUnits - 1];
+        } else {
+          numberSting = units[numberHundreds - 1] + " hundred ";
+        }
+      } return numberSting;
+
 }
